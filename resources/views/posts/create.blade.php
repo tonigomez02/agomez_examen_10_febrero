68d6de3c62cdd03167a5c9ce6a4485a1575b07c1
ToniGomez <agomezj@cifpfbmoll.eu>
2022-02-10 18:31:29 +0100

    3. Old values

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,12 +9,12 @@
 
             <div class="mb-3">
                 <label for="" class="form-label">@lang("Title")</label>
-                <input id="title" name="title" type="text" class="form-control"">
+                <input id="title" name="title" type="text" class="form-control" value="{{old("title")}}">
             </div>
 
             <div class="mb-3">
                 <label for="" class="form-label">@lang("Summary")</label>
-                <input id="summary" name="summary" type="text" class="form-control"  >
+                <input id="summary" name="summary" type="text" class="form-control"  value="{{old("summary")}}">
             </div>
 
             <div class="mb-3">
@@ -25,13 +25,13 @@
             <div class="mb-3">
                 <p class="text-primary fs-5">@lang("Options")</p>
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="expired" name="expired">
+                    <input class="form-check-input" type="checkbox" value="expired" name="expired" {{ old('expired') == 'expired' ? 'checked' : '' }}>
                     <label class="form-check-label" for="">
                         @lang("Expired")
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="commentable" name="commentable">
+                    <input class="form-check-input" type="checkbox" value="commentable" name="commentable" {{ old('commentable') == 'commentable' ? 'checked' : '' }}>
                     <label class="form-check-label" for="">
                         @lang("Commentable")
                     </label>
@@ -40,13 +40,13 @@
 
             <select class="form-select" aria-label="Default select example">
                 <option selected>Access</option>
-                <option value="1">Private</option>
-                <option value="2">Public</option>
+                <option value="private" {{ old('private') == 'private' ? 'checked' : '' }}>Private</option>
+                <option value="public" {{ old('public') == 'public' ? 'checked' : '' }}>Public</option>
             </select>
 
             <div class="mb-3">
                 <label for="" class="form-label">@lang("Date publication")</label>
-                <input id="date_publication" name="date_publication" type="date" class="form-control" >
+                <input id="date_publication" name="date_publication" type="date" class="form-control" {{old("summary")}}>
             </div>
 
             <div class="mt-4">
